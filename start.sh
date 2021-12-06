3e76e488622526dd42e59ccfe64a996bb2f2f6f6
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-12-06 10:35:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 python3 update.py
 if [[ -n $COMMANDS_URL ]]; then
-	apt install wget
 	rm bot/helper/telegram_helper/bot_commands.py
-	wget -q $COMMANDS_URL -O bot/helper/telegram_helper/bot_commands.py
+	curl -o bot/helper/telegram_helper/bot_commands.py $COMMANDS_URL
 fi
 python3 -m bot
