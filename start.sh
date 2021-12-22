9f919e8d90ce89ec897d7d6fea3e53f6722bb653
Tiararose Biezetta <92842340+tiararosebiezetta@users.noreply.github.com>
2021-12-22 08:21:18 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 python3 update.py
+
 if [[ -n $COMMANDS ]]; then
 	rm bot/helper/telegram_helper/bot_commands.py
 	echo "$COMMANDS" >> "bot/helper/telegram_helper/bot_commands.py"
 fi
+
 python3 -m bot
