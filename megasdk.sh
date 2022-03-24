9840ace379e4b5f70a1ff70cb577396cb4b98027
Iris <101499771+cuteiris@users.noreply.github.com>
2022-03-24 07:26:43 +0700

    Buildpack build (#1)

---
+++ megasdk.sh
@@ -0,0 +1,1 @@
+git clone https://github.com/meganz/sdk.git --depth=1 -b v3.9.1 /home/sdk && cd /home/sdk && rm -rf .git && autoupdate -fIv && ./autogen.sh && ./configure --disable-silent-rules --enable-python --with-sodium --disable-examples && make -j$(nproc --all) && cd bindings/python/ && python3 setup.py bdist_wheel && cd dist/ && pip3 install --no-cache-dir megasdk-3.9.1-*.whl # buildkit
--- start.sh
+++ start.sh
@@ -1,1 +1,38 @@
-python3 update.py && python3 -m bot
+pip install wheel
+pip install anytree
+pip install appdirs
+pip install aria2p
+pip install attrdict
+pip install beautifulsoup4
+pip install bencoding
+pip install cfscrape
+pip install feedparser
+pip install flask
+pip install google-api-python-client
+pip install google-auth-httplib2
+pip install google-auth-oauthlib
+pip install gunicorn
+pip install lk21
+pip install lxml
+pip install pillow
+pip install psutil
+pip install psycopg2-binary
+pip install pybase64
+pip install pyrogram
+pip install python-dotenv
+pip install python-magic
+pip install python-telegram-bot
+pip install qbittorrent-api
+pip install requests
+pip install speedtest-cli
+pip install six
+pip install telegraph
+pip install tenacity
+pip install tgCrypto
+pip install urllib3
+pip install yt-dlp
+
+bash megasdk.sh
+cd /app
+python3 update.py
+python3 -m bot
